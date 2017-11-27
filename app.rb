ebaed77683b1f1164ffd720d11353e5a201a8c8d
davicitoafc <d.cisneros849@gmail.com>
2017-11-27 18:26:29 -0500

    building up pages

--- app.rb
+++ app.rb
@@ -7,8 +7,6 @@ $: << File.join(File.dirname(__FILE__),"","thinkify_api")
 #require our library
 require 'thinkifyreader'
 require 'sinatra'
-require 'haml'
-
 
 # Create a reader to work with.
 # On windows you can just call .new and the class will scan for the first reader it can find (upto com20)
@@ -19,7 +17,7 @@ require 'haml'
    r = ThinkifyReader.new #Windows
 
    r.reading_active=false
-
+# Route for main page
    get '/' do
      @time = Time.now
      @version = r.version
@@ -28,48 +26,62 @@ require 'haml'
      erb :index
    end
 
-  puts "Query the reader's firmware version."
-	puts "Reader's Firmware Version: "
-  puts "#{r.version}"
+   get '/taglist' do
+    @reading_active = r.reading_active
 
-# Show the inventory parameters
-	puts
-	puts "Inventory Parameters:"
-  puts "#{r.inventory_params}"
+    @tag_list = r.tag_list.filter(/.*/).sort
+    r.tag_list.clear
+    erb :taglist 
+   end
 
+   get '/execute' do
+    erb :execute
+   end
 
-# Turn the reader on (start reading tags using default parameters)
-  puts
-  puts "Reading Tags:"
-  r.reading_active=true
+   post '/execute' do
+    command = (params["reader_command"].downcase)
+    @reader_response = r.execute(command).gsub("\r", "<br>")
+    erb :execute
+   end
 
-	begin
-
-			# Read for a few seconds...
-			sleep(3)
-
-			# The reader will put the tags it finds into its tag_list... An array of tags.
-			puts "Total Tags: #{r.tag_list.length}"
-
-			# Report what it found...
-			r.tag_list.each do |tag|
-				puts "EPC: #{tag.epc}"
-        puts "Count:  #{tag.count}"
-        puts "Time of Discovery: #{tag.disc}"
-        puts "Signal Strength: #{tag.rssi}"
-        puts "last time tag was read #{tag.last}"
-			end
-
-			# Clear its tag list.
-			r.tag_list.clear
-
-	rescue Exception
-
-	  puts "Exception Thrown."
-
-	ensure
-
-		#Turn off reading.
-		r.reading_active=false
-
-	end
+# # Show the inventory parameters
+# 	puts
+# 	puts "Inventory Parameters:"
+#   puts "#{r.inventory_params}"
+#
+#
+# # Turn the reader on (start reading tags using default parameters)
+#   puts
+#   puts "Reading Tags:"
+#   r.reading_active=true
+#
+# 	begin
+#
+# 			# Read for a few seconds...
+# 			sleep(3)
+#
+# 			# The reader will put the tags it finds into its tag_list... An array of tags.
+# 			puts "Total Tags: #{r.tag_list.length}"
+#
+# 			# Report what it found...
+# 			r.tag_list.each do |tag|
+# 				puts "EPC: #{tag.epc}"
+#         puts "Count:  #{tag.count}"
+#         puts "Time of Discovery: #{tag.disc}"
+#         puts "Signal Strength: #{tag.rssi}"
+#         puts "last time tag was read #{tag.last}"
+# 			end
+#
+# 			# Clear its tag list.
+# 			r.tag_list.clear
+#
+# 	rescue Exception
+#
+# 	  puts "Exception Thrown."
+#
+# 	ensure
+#
+# 		#Turn off reading.
+# 		r.reading_active=false
+#
+# 	end
--- db/mongodb.rb
+++
@@ -1,7 +0,0 @@
-require 'mongo'
-require './models/tags'
-
-#Creating a connection to database
-client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
-
-puts "#{client.cluster.servers.inspect}"
